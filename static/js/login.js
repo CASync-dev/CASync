3b06e23ad2ef36e85653ff786a016b799ef74193
Liam Cervenka <Liam@cervenka.space>
2026-03-30 22:03:12 +0800

    made a temproary auth implmentation (!just for testing). It just oggles log in or log out based on those buttons

--- static/js/login.js
+++ static/js/login.js
@@ -2,17 +2,17 @@
 // This will change when we figure authentication out! :)
 
 function loginImplementation() {
-    let user = document.forms["login"]["user"].value;
-    let pw = document.forms["login"]["pw"].value;
+  let user = document.forms['login']['user'].value;
+  let pw = document.forms['login']['pw'].value;
 
-    if (user == "") {
-        alert("Please enter your username.");
-        return false;
-    }
-    if (pw == "") {
-        alert("Please enter your password.");
-        return false;
-    }
-    alert("Login submission has not been implemented. Look out in the future!");
+  if (user == '') {
+    alert('Please enter your username.');
     return false;
+  }
+  if (pw == '') {
+    alert('Please enter your password.');
+    return false;
+  }
+  window.location.href = '/dev/login';
+  return false;
 }
