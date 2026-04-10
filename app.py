2a19d62daf6135a7d51e0d5d36495b8fd1c034ba
stoveup <uhoh0694@gmail.com>
2026-04-10 18:26:23 +0800

    Added fix to issue where flask could not migrate or upgrade database

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from datetime import datetime
 
 from flask import Flask, render_template, jsonify, request, session, redirect, url_for
+from flask_migrate import Migrate, upgrade
 import os
-from flask import Flask, render_template, jsonify, session, redirect, url_for
-from flask_migrate import Migrate
 from extensions import db
 from models import Calendar, User, Event
 from services.ical import import_ical
@@ -14,6 +13,9 @@ app.secret_key = os.getenv('SECRET_KEY', 'dev-secret-key')  # swap for real env 
 # Configure the database URI and initialize the database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 db.init_app(app)
+migrate = Migrate(app, db)
+
+
 migrate = Migrate(app, db)
 
 
