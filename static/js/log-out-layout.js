402e4d8fc38f3f9f3c9c193f01f09aec61adcfd0
stoveup <uhoh0694@gmail.com>
2026-04-13 18:42:15 +0800

    Added functionable mobile sidebar

--- static/js/log-out-layout.js
+++ static/js/log-out-layout.js
@@ -1,10 +1,16 @@
-// JS for layout for logged-out pages (homepage, faq)
-// Compeltely broken RIP will fix later
-const menubutton = document.getElementById("mobile-menu-button-open");
-menubutton.addEventListener("click", mobileMenu);
+// For now the click event is embedde into log-out-layout.html for simplicity.
+// May change in the future
 
+// Loads the mobile sidebar
 function mobileMenu() {
     const popout = document.getElementById("mobile-menu");
     popout.style.display = "block";
+
+}
+
+// Unloads mobile sidebar
+function closeMobileMenu() {
+    const popout = document.getElementById("mobile-menu");
+    popout.style.display = "none";
 
 }
