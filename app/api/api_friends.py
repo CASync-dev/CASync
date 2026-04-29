db778ffce34e3994f8819d73fdb229f1d8940410
stoveup <uhoh0694@gmail.com>
2026-04-29 23:05:47 +0800

    Removed debugging print statements

--- app/api/api_friends.py
+++ app/api/api_friends.py
@@ -13,11 +13,9 @@ def getusers():
     # Email search: Must be exact for privacy reasons
     data = request.get_json()
     if 'search' not in data or len(data['search']) < 1:
-        print("invalid search")
         return jsonify({"Error: Invalid search"}), 400
     # If it's an email
     if '@' in data['search']:
-        print("Email detected!")
         searchmail = data['search']
         query = text("SELECT users.username FROM users WHERE users.email = :e")
         param = {"e":searchmail}
@@ -26,25 +24,18 @@ def getusers():
         # .scalar in case email entered does not have an associated acc
         mail = db.session.scalar(query, param)
         if mail == None:
-            print("No email found!")
             return jsonify({'results': 0})
         mail = [mail]
-        print("Email found!")
         return jsonify({'results': mail})
     else:
-        print("Username detected!")
         # ie. Username
         user = data['search']
-        print(user)
         if len(user) < 3:
-            print("No user found!")
             # Prevent users from searching using less than 3 letters
             return jsonify({'results': 0})
-        print("User found!")
         query = text("SELECT users.username FROM users WHERE users.username = :u_")
         param = {"u_":user}
         users = list(db.session.scalars(query, param).all())
         if users == []:
             return jsonify({'results':0})
-        print(users)
         return jsonify({'results': users})
