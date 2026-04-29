4d1119a9e6e65db5468308d498c5d23b99f2c303
stoveup <uhoh0694@gmail.com>
2026-04-29 22:02:30 +0800

    Fixed api

--- app/api/api_friends.py
+++ app/api/api_friends.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required
-from sqlalchemy import select
+from sqlalchemy import select, text
 from app import db
 from app.models import User
 
@@ -11,26 +11,37 @@ api_friends = Blueprint("api_friends", __name__)
 def getusers():
     # Username search: 3 letters should give a range of usernames with those three letters
     # Email search: Must be exact for privacy reasons
-    data = request.args or {}
+    data = request.get_json()
     if 'search' not in data or len(data['search']) < 1:
+        print("invalid search")
         return jsonify({"Error: Invalid search"}), 400
     # If it's an email
     if '@' in data['search']:
+        print("Email detected!")
         searchmail = data['search']
-        query = select(User.username).where(User.email == searchmail)
+        query = text("SELECT users.username FROM users WHERE users.email = :e")
+        param = {"e":searchmail}
         # There should only be one account per email, if an error raises then there's
         # Something wrong with the datebase...
         # .scalar in case email entered does not have an associated acc
-        mail = db.session.scalar(query)
+        mail = db.session.scalar(query, param)
         if mail == None:
+            print("No email found!")
             return jsonify({'results': 0})
+        print("Email found!")
         return jsonify({'results': mail})
     else:
+        print("Username detected!")
         # ie. Username
         user = data['search']
+        print(user)
         if len(user) < 3:
+            print("No user found!")
             # Prevent users from searching using less than 3 letters
             return jsonify({'results': 0})
-        query = select(User.username).where(User.username.startswith(user))
-        users = list(db.session.scalars(query).all())
+        print("User found!")
+        query = text("SELECT users.username FROM users WHERE users.username = :u_")
+        param = {"u_":user}
+        users = list(db.session.scalars(query, param).all())
+        print(users)
         return jsonify({'results': users})
