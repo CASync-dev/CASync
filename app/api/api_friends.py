155cebd2827efe28b7908e4869150e57c0b905b0
stoveup <uhoh0694@gmail.com>
2026-04-29 14:25:58 +0800

    sqlalchemy sql to search for usernames/emails similar/exactly like searched term

--- app/api/api_friends.py
+++ app/api/api_friends.py
@@ -1,5 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
 from flask_login import login_required
+from sqlalchemy import select
+
+from app.models import User
 
 api_friends = Blueprint("api_friends", __name__)
 
@@ -8,4 +11,16 @@ api_friends = Blueprint("api_friends", __name__)
 def getusers():
     # Username search: 3 letters should give a range of usernames with those three letters
     # Email search: Must be exact for privacy reasons
-    
+    data = request.args or {}
+    if 'search' not in data or len(data['search']) < 1:
+        return jsonify({"Error: Invalid search"}), 400
+    # If it's an email
+    if '@' in data['search']:
+        searchmail = data['search']
+        query = select(User.username).where(User.email == searchmail).first()
+
+    else:
+        # ie. Username
+        user = data['search']
+        query = select(User.username).where(user in User.username)
+    return jsonify({'results': query})
