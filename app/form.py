8235153adc28b318f2d32fe80a191b52133dc226
Liam Cervenka <Liam@cervenka.space>
2026-04-29 13:28:50 +0800

    move validation from route to forms.py

--- app/form.py
+++ app/form.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, URLField, DateField, TimeField, EmailField
 from wtforms.validators import EqualTo, InputRequired, Regexp, Length, ValidationError
 import re
+from app.models import User
 
 # Custom validator to check for a strong password
 def strong_password(form, field):
@@ -31,11 +32,18 @@ class LoginForm(FlaskForm):
 
 class RegisterForm(FlaskForm):
     email = EmailField(validators=[InputRequired(message="All fields are required.")])
-    username = StringField(validators=[InputRequired(message="All fields are required."), Regexp('^[A-Za-z0-9_]+$', message="Username must contain only letters, numbers, and underscores.")])
+    username = StringField(validators=[InputRequired(message="All fields are required."), Regexp('^[A-Za-z0-9_]+$', message="Username must contain only letters, numbers, and underscores.")]),
     password = StringField(validators=[InputRequired(message="All fields are required."),strong_password])
     repeat_password = StringField(validators=[InputRequired(message="All fields are required."), EqualTo('password', message='Passwords must match')])
     submit = SubmitField('Register')
 
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already taken.')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
     # Override the validate method to ensure that duplicate error messages are not added to the form
     def validate(self, extra_validators=None):
         result = super().validate(extra_validators)
