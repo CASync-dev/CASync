a6aab2adb8d33d28a87c51d749120eda3e6673f1
stoveup <uhoh0694@gmail.com>
2026-05-03 23:03:12 +0800

    Implemented change password and account deletion features

--- app/loggedin/loggedin.py
+++ app/loggedin/loggedin.py
@@ -1,9 +1,11 @@
 from datetime import datetime, timedelta
-from flask import Blueprint, app, render_template, flash
+from flask import Blueprint, app, redirect, render_template, flash, url_for
 from flask_login import current_user, login_required
+from sqlalchemy import update
 from app.form import EventForm, IcalImportForm, accountDelForm, changePasswordForm
 from app.models import Calendar, User
 from services.ical import import_ical
+from app import db
 
 
 loggedin = Blueprint('loggedin', __name__, template_folder='../templates/loggedin', static_folder='../static')
@@ -46,6 +48,7 @@ def settings():
     form = IcalImportForm()
     changePassform = changePasswordForm()
     acdform = accountDelForm()
+    # iCal Form Validation
     if form.validate_on_submit():
         url = form.ical_url.data
         result, error = import_ical(url, current_user.id)
@@ -55,7 +58,26 @@ def settings():
             imported = result.get('imported') or result.get('created', 0)
             updated = result.get('updated', 0)
             flash(f"Imported {imported} events, updated {updated}.", "success")
-
+    # Change Password Form Validation
+    if changePassform.validate_on_submit():
+        current = changePassform.current_password.data
+        if current_user.verify_password(current):
+            current_user.password = changePassform.new_password.data
+            db.session.flush()
+            db.session.commit()
+            flash(f"Successfully changed user's password.", "success")
+        else:
+            flash(f"Error changing password: Incorrect password.", "error")
+    # Account Deletion form Validation
+    if acdform.validate_on_submit():
+        if current_user.email != acdform.email.data:
+            flash(f'Error in account deletion: Incorrect email.', "error")
+        elif current_user.username != acdform.username.data:
+            flash(f'Error in account deletion: Incorrect username.', "error")
+        elif not current_user.verify_password(acdform.password.data):
+            flash(f'Error in account deletion: Incorrect password.', "error")
+        else:
+            redirect(url_for('api_users.accountdeletion'))
     syncs = Calendar.query.filter_by(user_id=current_user.id).order_by(Calendar.synced_at.desc()).first()
     last_synced = syncs.synced_at if syncs else "Never"
 
