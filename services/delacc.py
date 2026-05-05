c9ec2bbaa42359217788b69c452a41c65ce4ee53
stoveup <uhoh0694@gmail.com>
2026-05-05 20:42:06 +0800

    Delete account now also deleted the pfp associated with the account

--- services/delacc.py
+++ services/delacc.py
@@ -1,3 +1,6 @@
+import os
+
+from flask import current_app
 from sqlalchemy import delete
 
 from app import db
@@ -7,12 +10,13 @@ def removeUser(userid):
     '''
     Parameters:
     - userid: id of user to be removed.
+    1. Delete PFP associated with User, if one exists.
 
-    1. Delete Events associated with ical links associated with the user
+    2. Delete Events associated with ical links associated with the user
 
-    2. Delete iCal Links associated with user
+    3. Delete iCal Links associated with user
 
-    3. Delete User
+    4. Delete User
 
     ps. Shouldn't need to remove groups with the user.
     '''
@@ -20,13 +24,18 @@ def removeUser(userid):
     calendars = Calendar.query.filter(Calendar.user_id == userid).all()
     events = Event.query.filter(Event.user_id == userid).all()
 
-    # 1. Delete Events
+    # 1. Delete PFP ( Might be redundant to change the bool when we're about to delete the user, but just a precaution. )
+    if user.avatarurl:
+        os.remove(os.path.join(current_app.config['UPLOAD_PATH'], str(user.id)))
+        user.avatarurl = False
+
+    # 2. Delete Events
     delEvents = delete(Event).where(Event.user_id == userid)
     db.session.execute(delEvents)
-    # 2. Delete Calendars
+    # 3. Delete Calendars
     delCal = delete(Calendar).where(Calendar.user_id == userid)
     db.session.execute(delCal)
-    # 3. Delete User
+    # 4. Delete User
     delUser = delete(User).where(User.id == userid)
     db.session.execute(delUser)
 
