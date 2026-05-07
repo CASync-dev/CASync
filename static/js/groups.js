5b3bd78250f5601588a3bd5dd09845ce547f3887
Kelly-Vl <kelly.valencia2006@gmail.com>
2026-05-07 23:55:46 +0800

    Added TODO to sort out what to do next

--- static/js/groups.js
+++ static/js/groups.js
@@ -1,57 +1,64 @@
-  let groupname = "My Group";
-  let grouplist = [];
-  function loadcreategroup() {
-    const x = document.getElementById("create-group");
-    x.showModal();
-  }
-  function closeloadcreategroup() {
-    const x = document.getElementById("create-group");
-    x.close();
-  }
-  function loadselectfriends(){
-    const x = document.getElementById("create-group");
-    groupname = document.getElementById("group-name-input").value;
-    x.close();
-    const y = document.getElementById("select-friend");
-    y.showModal();
-    document.getElementById("gname").innerText = groupname;
-  }
-  function closeloadselectfriends() {
-    const x = document.getElementById("select-friend");
-    // Reset all saved values.
-    groupname = "";
-    grouplist = [];
-    x.close();
-  }
+// Variables
+let groupname = "My Group";
+let grouplist = [];
 
-  // Dev function to check data 
-  function testsubmit() {
-    alert(groupname);
-    alert(grouplist);
-  }
+// Functions for Group Dialog Modal
+function loadcreategroup() {
+  const x = document.getElementById("create-group");
+  x.showModal();
+}
+function closeloadcreategroup() {
+  const x = document.getElementById("create-group");
+  x.close();
+}
 
-  function submitgroupcreation() {
-    let url = "/api/group/create";
-    fetch(url, {
-      method: 'POST',
-      body: JSON.stringify({name: groupname, list: grouplist})
-    });
-    // Gotta add a error catch here for any group creation issues!
-  }
+// Functions for Friends Dialog Modal
+function loadselectfriends() {
+  const x = document.getElementById("create-group");
+  groupname = document.getElementById("group-name-input").value;
+  x.close();
+  const y = document.getElementById("select-friend");
+  y.showModal();
+  document.getElementById("gname").innerText = groupname;
+}
+function closeloadselectfriends() {
+  const x = document.getElementById("select-friend");
+  // Reset all saved values.
+  groupname = "";
+  grouplist = [];
+  x.close();
+}
 
-  // Finding friends script courtesy of Liam
-  const DEV_USERS = [
-    "alice_wonder",
-    "bob_builder",
-    "charlie_chaplin",
-    "diana_prince",
-    "eve_online",
-  ];
+// TODO: Remember to actually change this later so that groups will show up on page
+// Dev function to check data
+function testsubmit() {
+  alert(groupname);
+  alert(grouplist);
+}
 
-  function searchFriends() {
-    const list = document.getElementById("friend-search-list");
-    list.innerHTML = DEV_USERS.map(
-      (u) => `
+function submitgroupcreation() {
+  let url = "/api/group/create";
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify({ name: groupname, list: grouplist }),
+  });
+  // Gotta add a error catch here for any group creation issues!
+}
+
+// TODO: Yoinking Search Friends from the finished Friends Page
+// Finding friends script courtesy of Liam
+const DEV_USERS = [
+  "alice_wonder",
+  "bob_builder",
+  "charlie_chaplin",
+  "diana_prince",
+  "eve_online",
+];
+
+function searchFriends() {
+  const list = document.getElementById("friend-search-list");
+  list.innerHTML = DEV_USERS.map(
+    (u) => `
       <li class="flex items-center justify-between py-2 px-4 hover:bg-gray-100 rounded-md">
         <div class="flex items-center gap-3">
           <img src="https://placehold.co/200x200" class="h-8 w-8 rounded-full" />
@@ -59,26 +66,26 @@
         </div>
         <button id = '${u}' onclick="return added(this)" class="text-sm bg-primary text-white px-3 py-1 rounded-md hover:bg-blue-800 cursor-pointer">+</button>
       </li>`,
-    ).join("");
-    document.getElementById("friend-search-results").classList.remove("hidden");
-  }
+  ).join("");
+  document.getElementById("friend-search-results").classList.remove("hidden");
+}
 
-  function added(button) {
-    if (button.innerHTML == '+') {
-      button.innerHTML = '-';
-      button.classList.add('bg-red-600');
-      button.classList.add('hover:bg-red-800');
-      button.classList.remove('bg-primary');
-      button.classList.remove('hover:bg-blue-800');
-      const idToPush = String(button.id);
-      grouplist.push(idToPush);
-      return
-      }
-    button.innerHTML = '+';
-    button.classList.add('bg-primary');
-    button.classList.add('hover:bg-blue-800');
-    button.classList.remove('bg-red-600');
-    button.classList.remove('hover:bg-red-800');
-    const idToPop = String(button.id);
-    grouplist.pop(idToPop);
+function added(button) {
+  if (button.innerHTML == "+") {
+    button.innerHTML = "-";
+    button.classList.add("bg-red-600");
+    button.classList.add("hover:bg-red-800");
+    button.classList.remove("bg-primary");
+    button.classList.remove("hover:bg-blue-800");
+    const idToPush = String(button.id);
+    grouplist.push(idToPush);
+    return;
   }
+  button.innerHTML = "+";
+  button.classList.add("bg-primary");
+  button.classList.add("hover:bg-blue-800");
+  button.classList.remove("bg-red-600");
+  button.classList.remove("hover:bg-red-800");
+  const idToPop = String(button.id);
+  grouplist.pop(idToPop);
+}
