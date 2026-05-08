ad584605fa2e59095561970115e4f4baa5c6d0a9
stoveup <uhoh0694@gmail.com>
2026-05-08 21:29:30 +0800

    Renamed file to test_user since it'll be used to test User table

--- test/test_user.py
+++ test/test_user.py
@@ -1,22 +1,21 @@
-import os
-os.environ['DATABASE_URL'] = 'sqlite:///:memory'
-# Do tests on this non-persistent database
-
-from datetime import datetime, timezone, timedelta
 import unittest
-from app import app, db
+from app import create_app, db
+from app.config import TestConfig
 from app.models import User
 from app.loggedout.loggedout import register, login
 
 # Tests User functionability: registering, logging, password hashing, etc.
 class UserTestCase(unittest.TestCase):
     def setUp(self):
-        self.app_context = app.app_context()
+        self.app = create_app(config_class=TestConfig())
+        self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
+        self.client = self.app.test_client()
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
-
+        self.app = None
+        self.app_context = None
