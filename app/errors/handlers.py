5335f9abef953fc9358382ae91af90c704ffa77d
stoveup <uhoh0694@gmail.com>
2026-05-08 19:13:59 +0800

    Fixed minor issue with 404 not showing the right page

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from app.errors import error
 
-@error.errorhandler(404)
+@error.app_errorhandler(404)
 def page_not_found(e):
     return render_template("errors/404.html"), 404
 
