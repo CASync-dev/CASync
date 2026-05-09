a0eb204e72dc4e1f9db16c035f3fdcd13dd3d539
Kelly-Vl <kelly.valencia2006@gmail.com>
2026-05-09 15:27:36 +0800

    Added api function for creating groups

--- app/api/api_groups.py
+++ app/api/api_groups.py
@@ -8,21 +8,40 @@ from app.models import Group, User
 api_groups = Blueprint("api_groups", __name__)
 
 
-# Waiting until friends are implemented
-@api_groups.route("/api/group/create", methods=['POST'])
-@login_required
-def create_group():
-    data = request.get_json()
-    group = Group(group_name = data['name'])
-    for name in data['list']:
-        print("Not finished, here to get rid of stupid python error")
-
-@api_groups.route("/api/group/friends", methods=['GET'])
+@api_groups.route("/api/group/friends", methods=["GET"])
 @login_required
 def group_get_friends():
-    friends = current_user.get_friends() # list of user objects
-    
+    friends = current_user.get_friends()  # list of user objects
+
     # Format response to JSON for browsers
-    return jsonify({
-        "friends": [friend.public_dict() for friend in friends]
-    }), 200
+    return jsonify({"friends": [friend.public_dict() for friend in friends]}), 200
+
+@api_groups.route("/api/group/create", methods=["POST"])
+@login_required
+def create_group():
+    # Reads JSON payload from frontend
+    data = request.get_json()
+
+    # Extracts data
+    name = data.get("name")  # name of new group
+    friends_added = data.get(
+        "list", []
+    )  # list of usernames/friends to include in group; missing = empty list
+
+    # Validate group name exist
+    if not name:
+        return jsonify({"error": "Group name required"}), 400
+
+    group = Group(group_name=name)
+    group.members.append(current_user)  # adds current user (creator of group) to group
+
+    # Adds the rest of friends to group
+    for friend_username in friends_added:
+        user = User.query.filter_by(username=friend_username).first()
+        if user and user != current_user:
+            group.members.append(user)  # populates many-to-many relationship
+
+    db.session.add(group)
+    db.session.commit()
+
+    return jsonify({"success": True, "group": group.to_dict()})
