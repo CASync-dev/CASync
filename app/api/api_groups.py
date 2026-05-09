81294b00a5cfbc992308011573f252684845c228
Kelly-Vl <kelly.valencia2006@gmail.com>
2026-05-09 19:27:22 +0800

    Added leave group function in api_groups.py

--- app/api/api_groups.py
+++ app/api/api_groups.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify
 from flask_login import current_user, login_required
 from sqlalchemy import select
 from app import db
-from app.models import Group, User
+from app.models import Group, User, user_group_association
 
 
 api_groups = Blueprint("api_groups", __name__)
@@ -45,3 +45,28 @@ def create_group():
     db.session.commit()
 
     return jsonify({"success": True, "group": group.to_dict()})
+
+# Handles users leaving the group
+@api_groups.route("/api/group/leave", methods=["POST"])
+@login_required
+def leave_group():
+    data = request.get_json()
+    if 'group_id' not in data:
+        return jsonify({"Error: Invalid group_id"}), 400
+    group_id = int(data['group_id'])
+
+    # Check if current user exists in the given group (find relationship row)
+    # Uses .delete because user_group_association is a raw SQL table, not a python ORM object
+    relationship_row = user_group_association.delete().where(
+        user_group_association.c.user_id  == current_user.id,
+        user_group_association.c.group_id == group_id
+    )
+
+    result = db.session.execute(relationship_row)
+
+    if result.rowcount == 0:
+        return jsonify({"Error": "You are not in this group"}), 404
+
+    db.session.commit()
+
+    return jsonify({"Message": "You have successfully left the group."}), 200
