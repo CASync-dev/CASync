452d7ce609e1bfa02d7b98a5041137d7ee65b168
stoveup <uhoh0694@gmail.com>
2026-05-10 20:27:07 +0800

    Added base functions for new test pythons

---
+++ unittests/test_groups.py
@@ -0,0 +1,20 @@
+import unittest
+from app import create_app, db
+from app.config import TestConfig
+
+class GroupTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app(config_class=TestConfig())
+        self.app.config['WTF_CSRF_ENABLED'] # Disables CSRF during tests
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        self.populate_db()
+        self.client = self.app.test_client()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+        self.app = None
+        self.app_context = None
