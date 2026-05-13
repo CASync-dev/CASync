becedab7f1c1970b2b03ab740008f6847494aeff
stoveup <uhoh0694@gmail.com>
2026-05-13 21:06:22 +0800

    Done accept and reject friend apis (not tested...ironically)

--- unittests/test_friends.py
+++ unittests/test_friends.py
@@ -1,5 +1,4 @@
 import unittest
-
 from flask_login import current_user, login_user
 from app import create_app, db
 from app.config import TestConfig
@@ -91,6 +90,55 @@ class FriendsTestCase(unittest.TestCase):
             'search': 'allen'
         })
         assert response.status_code == 200
+        assert response['results'] == { 2: 'allen'}
+
+        # Request has already been made and shouldn't show up
+        f = Friendship(sender_id=current_user.id, recipient_id=2, status='pending', created_at=db.func.now())
+        db.session.add(f)
+        db.session.commit()
+
+        # Testing searching when a request has already been made (Email)--------------------------
+        response = self.client.post('/api/getusers', json={
+            'search': 'friend@fun.net'
+        })
+        assert response.status_code == 200
+        assert response['results'] == 0
+
+        # User
+        response = self.client.post('/api/getusers', json={
+            'search': 'allen'
+        })
+        assert response.status_code == 200
+        assert response['results'] == 0
+        # ----------------------------------------------------------------------------------------
+        # Testing searching when request rejected.
+        f.status = 'rejected'
+        response = self.client.post('/api/getusers', json={
+            'search': 'friend@fun.net'
+        })
+        assert response.status_code == 200
+        assert response['results'] == { 2: 'allen'}
+
+        response = self.client.post('/api/getusers', json={
+            'search': 'allen'
+        })
+        assert response.status_code == 200
+        assert response['results'] == { 2: 'allen'}
+
+        # Testing searching when request accepted.
+        f.status = 'accepted'
+        f.accepted_at = db.func.now()
+        response = self.client.post('/api/getusers', json={
+            'search': 'friend@fun.net'
+        })
+        assert response.status_code == 200
+        assert response['results'] == 0
+
+        # User
+        response = self.client.post('/api/getusers', json={
+            'search': 'allen'
+        })
+        assert response.status_code == 200
         assert response['results'] == 0
 
     def test_requestfriend(self):
@@ -122,24 +170,26 @@ class FriendsTestCase(unittest.TestCase):
         assert response.status_code == 400
         assert response['error'] == "Friend request already pending"
 
-        # Should really test making req on a uesr who's already friends, but might be easier to do that in test_acceptfriend()
+        # User was rejected
+        fq = Friendship.query.filter(Friendship.recipient_id == 2).first()
+        fq.status = 'rejected'
+        db.session.commit(fq)
+        response = self.client.post('/api/requestfriend', json={
+            'user_id': '2'
+        })
+        assert response.status_code == 200
+        assert response['message'] == "Friend request sent to allen!"
+
+        # User is already friends
+        fq.status = 'accepted'
+        fq.accepted_at = db.func.now()
+        db.session.commit(fq)
+        response = self.client.post('/api/requestfriend', json={
+            'user_id': '2'
+        })
+        assert response.status_code == 400
+        assert response['error'] == "You are already friends"
         
-        # Testing searching when a request has already been made (Email)--------------------------
-        response = self.client.post('/api/getusers', json={
-            'search': 'friend@fun.net'
-        })
-        assert response.status_code == 200
-        assert response['results'] == 0
-
-        # User
-        response = self.client.post('/api/getusers', json={
-            'search': 'allen'
-        })
-        assert response.status_code == 200
-        assert response['results'] == 0
-        # ----------------------------------------------------------------------------------------
-
-
     def test_acceptfriend(self):
         # Creating the friendship explicitly incase previous test did not work. Probably not a good idea to rely on
         # Other apis for isolated tests :)
@@ -184,13 +234,114 @@ class FriendsTestCase(unittest.TestCase):
         assert response['message'] == "Friend request accepted from allen!"
         assert Friendship.query.filter(Friendship.id == request_id).first.status == 'accepted'
 
+        # Testing request already accepted! So should not show up again.
+        response = self.client.post('/api/acceptfriend', json={
+            'request_id': realfriendshipid
+        })
+        assert response.status_code == 404
+        assert response == {"Error: No pending friend request found"}
+
+
     def test_rejectfriend(self):
-        print()
+        # Creating the friendship explicitly incase previous test did not work. Probably not a good idea to rely on
+        # Other apis for isolated tests :)
+        new_request = Friendship(sender_id=2, recipient_id=current_user.id, status='pending', created_at=db.func.now()) # so that sender is our 'friend' user
+        user = User(username="coolnotfriend", email="abcde@fg.com")
+        user.password = 'foobar' # Not required but why not :')
+        db.session.add(user)
+        not_our_request = Friendship(sender_id=user.id, recipient_id=2, status='pending', created_at=db.func.now()) # Request that the current user did not send and not for.
+        db.session.add(new_request)
+        db.session.add(not_our_request)
+        db.session.commit()
+
+        # Test required field not in json
+        response = self.client.post('/api/rejectfriend', json={
+            'notrelated': 'to this api :('
+        })
+        assert response.status_code == 400
+        assert response == {"Error: Invalid request_id"}
+
+        # Testing request does not exist
+        response = self.client.post('/api/rejectfriend', json={
+            'request_id': 105
+        })
+        assert response.status_code == 404
+        assert response == {"Error: No pending friend request found"}
+
+        # Testing rejecting a request where the current user is not the recipient
+        notourid = not_our_request.id
+        response = self.client.post('/api/rejectfriend', json={
+            'request_id': notourid
+        })
+        assert response.status_code == 403
+        assert response == {"Error: You can only reject friend requests sent to you"}
+
+        # Testing actuals
+        realfriendshipid = new_request.id
+        response = self.client.post('/api/rejectfriend', json={
+            'request_id': realfriendshipid
+        })
+        assert response.status_code == 200
+        assert response['message'] == "Friend request rejected."
+
+        # Request rejected already, so can't reject again
+        response = self.client.post('/api/rejectfriend', json={
+            'request_id': realfriendshipid
+        })
+        assert response.status_code == 404
+        assert response == {"Error: No pending friend request found"}
 
     def test_removefriend(self):
-        print()
+        # Test required field not in json
+        response = self.client.post('/api/removefriend', json={
+            'notrelated': 'to this api :('
+        })
+        assert response.status_code == 400
+        assert response == {"Error: Invalid friend_id"}
 
+        # Friendship doesn't exist between the users.
+        response = self.client.post('/api/removefriend', json={
+            'friend_id': 2
+        })
+        assert response.status_code == 404
+        assert response == {"Error: You are not friends with this user"}
+
+        # Friendship has not been accepted yet
+        fq = Friendship(sender_id=current_user.id, recipient_id=2, status='pending', created_at=db.func.now(), accepted_at=db.func.now())
+        db.session.add(fq)
+        db.session.commit()
+        response = self.client.post('/api/removefriend', json={
+            'friend_id': 2
+        })
+        assert response.status_code == 404
+        assert response == {"Error: You are not friends with this user"}
+
+        # Friendship was rejected
+        fq.status = 'rejected'
+        db.session.add(fq)
+        db.session.commit()
+        response = self.client.post('/api/removefriend', json={
+            'friend_id': 2
+        })
+        assert response.status_code == 404
+        assert response == {"Error: You are not friends with this user"}
+
+        # Friendship exists & accepted
+        fq.status = 'accepted'
+        db.session.add(fq)
+        db.session.commit()
+        response = self.client.post('/api/removefriend', json={
+            'friend_id': 2
+        })
+        assert response.status_code == 200
+        assert response['message'] == "Friend removed."
+
+    # Hm..
     def test_friends_status(self):
-        print()
+        response = self.client.post('/api/friendsstatus', json={
+            'notrelated': 'to this api :('
+        })
+        assert response.status_code == 400
+        assert response == {"Error: Invalid friend_id"}
 
     
