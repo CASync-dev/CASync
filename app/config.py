89f90a93bed599f4f8470ecda15573930a974fce
stoveup <uhoh0694@gmail.com>
2026-05-28 22:55:15 +0800

    Removed hardcoded SECRET KEY

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     # Secret key for session logic
-    SECRET_KEY = os.getenv('SECRET_KEY') or 'DEV_KEY'
+    SECRET_KEY = os.getenv('SECRET_KEY')
 
     # Removed below so github actions can run the tests as well.
     #Flag error if missing secret key environment var:
